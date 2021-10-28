1dbf00d627010978bdf3bc9b4e55dde327cb6d36
Fernando Corrêa <fptcorrea@gmail.com>
2021-10-28 19:42:48 -0300

    subir o código que fiz na aula

--- pratica/4-base-dos-dados.R
+++ pratica/4-base-dos-dados.R
@@ -1,6 +1,15 @@
 library(dplyr)
 library(bigrquery)
-library(dplyr)
+
+# seguindo exatamente o que tem no site
+
+install.packages("basedosdados")
+library("basedosdados")
+# Defina o seu projeto no Google Cloud
+set_billing_id("<YOUR_PROJECT_ID>")
+# Para carregar o dado direto no R
+query <- "SELECT * FROM `basedosdados.br_inep_ideb.escola`"
+df <- read_sql(query)
 
 id_conta_do_google <- "live-curso-r-bd-2"
 
@@ -11,8 +20,23 @@ con <- dbConnect(
   billing = id_conta_do_google
 )
 
+escola_no_r_passo1 <- tbl(con, "escola") %>% 
+  filter(sigla_uf == "SP") %>% 
+  select(id_escola, sigla_uf, ano, id_municipio, ideb)
+
+analise_direto_na_nuvem <- tbl(con, "escola") %>% 
+  sample_n(size = 100)
+
+analise_direto_na_nuvem
+
+escola_no_r_passo1
+
+escola_no_r_passo2 <- collect(escola_no_r_passo1)
+
+escola_no_r_passo2
+
 # tabela
-escola <- tbl(conexao_ideb, "escola") %>%
+escola <- tbl(con, "escola") %>%
   filter(sigla_uf == "SP") %>% 
   group_by(ano, estado_abrev, municipio) %>%
   summarise(ideb = mean(ideb, na.rm = TRUE)) %>%
